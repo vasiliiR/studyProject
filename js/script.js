bfaa37de49f447d4aee00c9b9788c7f62636fb95
Vasilii <vasiliirvv@gmail.com>
2022-11-26 11:58:45 +0300

    first commit i stady coding

---
+++ js/script.js
@@ -0,0 +1,25 @@
+"use strict"; // strict mode for spelling code
+/*
+const obj = {
+    name: "Vasilii",
+    age : 25,
+};
+
+console.log(obj.name);
+
+obj.vacantion = "01 february";
+console.log(obj.vacantion);
+
+
+let storeName = "enter";
+const storeDescription = {
+    budget : 10000,
+    employees : ['Ann', 'Rick', 'Cris'],
+    products : {
+        iphone8 : 5000,
+        ipone13Mini : 18000,
+    },
+    open : true,
+};*/
+
+alert("hello");
